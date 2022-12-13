aa7f5b27cce9e72549d5f3bf16669658a6fb2b5c
XinyuP <xinyup@umich.edu>
2022-12-13 02:25:24 -0500

    add another http request to fetch geocoding api

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
 const request = require('postman-request');
 
+
+// geocoding
+// take an address and convert that into a lati/longi coordinate pair
+
+// Address -> Lat/Long -> Weather
+
+// Goal: print the lat/long for Los Angeles
+const geocodeUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoieGlueXVwYW5nIiwiYSI6ImNrdTVuZmN0NjI2bnYyb3BtYWNyYThtaGIifQ.WK5_bWckr1Qup2wd3_49sA&limit=1';
+
+request({ url: geocodeUrl, json: true }, (error, response) => {
+
+    const lat = response.body.features[0].center[1]
+    const long = response.body.features[0].center[0]
+
+    console.log(response.body)
+    console.log("The lat/long of " + response.body.features[0].place_name + " is " + lat + ", " + long)
+})
+
+
+
+
 const url =
 	'http://api.weatherstack.com/current?access_key=48ff11e343fef3921bb5cb441725bdf2&query=37.8267,%20-122.4233&units=f';
 
 
-// request({ url: url }, (error, response) => {
-//     const data = JSON.parse(response.body)
-//     console.log(data.current)
-// });
+request({ url: url }, (error, response) => {
+    const data = JSON.parse(response.body)
+    console.log(data.current)
+});
 
 
 // request automatically parse the JSON for us
